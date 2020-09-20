1a3ac641443031f8f77037d408c5a0f27d082d60
li_wei <li_wei@hisuntech.com>
2020-09-20 18:28:27 +0800

    创建分支

--- hellogit/src/main/java/com/atguigu/git/HelloGit.java
+++ hellogit/src/main/java/com/atguigu/git/HelloGit.java
@@ -4,6 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git 哈哈");
         System.out.println("更新1");
-
+        System.out.println("创建分支");
     }
 }
