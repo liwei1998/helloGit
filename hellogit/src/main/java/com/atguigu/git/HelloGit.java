546a25d423977b038a44adb7d902156f3c41dd04
li_wei <li_wei@hisuntech.com>
2020-09-20 21:30:44 +0800

    11

--- hellogit/src/main/java/com/atguigu/git/HelloGit.java
+++ hellogit/src/main/java/com/atguigu/git/HelloGit.java
@@ -2,14 +2,7 @@ package com.atguigu.git;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("11111");
-        System.out.println("更新1");
-        System.out.println("创建分支");
-        System.out.println("主干添加");
-        System.out.println("分支添加222");
-        System.out.println("主干添加2");
-        System.out.println("分支添加");
-        System.out.println("分支添加2改成33333");
-        System.out.println("在线更新");
+        System.out.println("hello git 哈哈");
+
     }
 }
